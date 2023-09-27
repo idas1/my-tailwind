4d0e9fc22e26d28a9a087f21bb2bf055906cb32c
tiendung <tiendunga2k64@gmail.com>
2023-09-27 17:56:56 +0700

    add

---
+++ src/main.js
@@ -0,0 +1,211 @@
+function changeProfile() {
+  let contElement = document.getElementById("content");
+  contElement.innerHTML = `
+    <div class="container-item h-max w-11/12 mx-auto flex">
+    <div class="item-about w-1/3 bg-slate-50 rounded-lg h-max mx-2">
+      <ul class="flex-col px-8 py-8">
+        <li class="py-2">Full Name:
+          John Doe</li>
+        <li class="py-2">Status:
+          Active</li>
+        <li class="py-2">Role:
+          Developer</li>
+        <li class="py-2">Country:
+          USA</li>
+        <li class="py-2">Languages:
+          English</li>
+        <li class="py-2">Contact:
+          (123) 456-7890</li>
+        <li class="py-2">Skype:
+          john.doe</li>
+        <li class="py-2">Email:
+          john.doe@example.com</li>
+      </ul>
+    </div>
+    <div class="item-activity w-2/3 bg-slate-50 rounded-lg max-h-max mx-2 px-4 py-4">
+      <h2>Activity TimeLine</h2>
+      <div class="timeLine flex">
+        <ul>
+          <li class="my-2">Client Meeting</li>
+          <li class="my-2">Create a new project for client</li>
+          <li class="my-2">Shared 2 New Project Files</li>
+          <li class="my-2">Project status updated</li>
+          <li class="my-2">Woocommerce iOS App Completed</li>
+        </ul>
+        
+      </div>
+    </div>
+    
+  </div>
+    `;
+}
+
+function changeTeam() {
+  let teamElement = document.getElementById("content");
+  teamElement.innerHTML = `
+    <div class="team h-max w-11/12 mx-auto flex flex-wrap px-1 py-1">
+            <div class="item-member p-4 mt-4 mx-1 h-max bg-slate-50 rounded-lg " style="width: 30%;">
+              <div class="flex place-content-evenly">
+              <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+              <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+              </div>
+              <h1>React Developer</h1>
+              <p>We don’t make assumptions about the rest of your technology stack </p>
+            </div>
+            <div class="item-member p-4 mt-4 mx-1 max bg-slate-50 rounded-lg " style="width: 30%;">
+            <div class="flex place-content-evenly">
+            <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+            <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+            </div>
+              <h1>React Developer</h1>
+              <p>We don’t make assumptions about the rest of your technology stack </p>
+            </div>
+            <div class="item-member p-4 mt-4 mx-1 max bg-slate-50 rounded-lg " style="width: 30%;">
+            <div class="flex place-content-evenly">
+            <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+            <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+            </div>
+              <h1>React Developer</h1>
+              <p>We don’t make assumptions about the rest of your technology stack </p>
+            </div>
+            <div class="item-member p-4 mt-4 mx-1 max bg-slate-50 rounded-lg " style="width: 30%;">
+            <div class="flex place-content-evenly">
+            <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+            <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+            </div>
+              <h1>React Developer</h1>
+              <p>We don’t make assumptions about the rest of your technology stack </p>
+            </div>
+            <div class="item-member p-4 mt-4 mx-1 max bg-slate-50 rounded-lg " style="width: 30%;">
+            <div class="flex place-content-evenly">
+            <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+            <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+            </div>
+              <h1>React Developer</h1>
+              <p>We don’t make assumptions about the rest of your technology stack </p>
+            </div>
+            <div class="item-member p-4 mt-4 mx-1 max bg-slate-50 rounded-lg " style="width: 30%;">
+            <div class="flex place-content-evenly">
+            <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+            <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+            </div>
+              <h1>React Developer</h1>
+              <p>We don’t make assumptions about the rest of your technology stack </p>
+            </div>
+          </div>
+    `;
+}
+
+function changeProject() {
+  let proElement = document.getElementById("content");
+  proElement.innerHTML = `
+      <div class="team h-max w-11/12 mx-auto flex flex-wrap px-1 py-1">
+              <div class="item-member p-4 mt-4 mx-1 h-36 bg-slate-50 rounded-lg " style="width: 30%;">
+                <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+                <h1>React Developer</h1>
+                <p>We don’t make assumptions about the rest of your technology stack </p>
+              </div>
+              <div class="item-member p-4 mt-4 mx-1 h-36 bg-slate-50 rounded-lg " style="width: 30%;">
+                <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/vue-label.png" alt="">
+                <h1>React Developer</h1>
+                <p>We don’t make assumptions about the rest of your technology stack </p>
+              </div>
+              <div class="item-member p-4 mt-4 mx-1 h-36 bg-slate-50 rounded-lg " style="width: 30%;">
+                <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/xd-label.png" alt="">
+                <h1>React Developer</h1>
+                <p>We don’t make assumptions about the rest of your technology stack </p>
+              </div>
+              <div class="item-member p-4 mt-4 mx-1 h-36 bg-slate-50 rounded-lg " style="width: 30%;">
+                <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/social-label.png" alt="">
+                <h1>React Developer</h1>
+                <p>We don’t make assumptions about the rest of your technology stack </p>
+              </div>
+              <div class="item-member p-4 mt-4 mx-1 h-36 bg-slate-50 rounded-lg " style="width: 30%;">
+                <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/react-label.png" alt="">
+                <h1>React Developer</h1>
+                <p>We don’t make assumptions about the rest of your technology stack </p>
+              </div>
+              <div class="item-member p-4 mt-4 mx-1 h-36 bg-slate-50 rounded-lg " style="width: 30%;">
+                <img class="rounded-3xl h-10"  src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/icons/brands/figma-label.png" alt="">
+                <h1>React Developer</h1>
+                <p>We don’t make assumptions about the rest of your technology stack </p>
+              </div>
+            </div>
+      `;
+}
+
+function changeConnect() {
+    let connectElement = document.getElementById("content");
+    connectElement.innerHTML = `
+    <div class="connection h-max w-11/12 mx-auto flex flex-wrap px-1 py-1">
+    <div style="width: 30%;" class="connect-item max-h-max mt-2 rounded-lg mx-3 px-3 py-4 bg-white flex flex-col items-center text-center">
+        <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+        <div class="con-name font-medium text-2xl">Mark Gilbert</div>
+        <div class="job-name text-zinc-500">UI Designner</div>
+        <div class="skill my-2">
+            <a href="#" class="rounded-xl px-2 py-1 text-slate-700 bg-slate-400">Figma</a>
+            <a href="#" class=" rounded-xl px-2 py-1 text-orange-500 bg-orange-300">Sketch</a>
+        </div>
+        <div class="describle">Im get 3 exp, develop new features SO YOU CAN in React.</div>
+        <button class="px-4 py-1 bg-violet-300 text-white rounded-xl">Connected</button>
+    </div>
+    <div style="width: 30%;" class="connect-item max-h-max mt-2 rounded-lg mx-3 px-3 py-4 bg-white flex flex-col items-center text-center">
+        <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+        <div class="con-name font-medium text-2xl">Mark Gilbert</div>
+        <div class="job-name text-zinc-500">UI Designner</div>
+        <div class="skill my-2">
+            <a href="#" class="rounded-xl px-2 py-1 text-slate-700 bg-slate-400">Figma</a>
+            <a href="#" class=" rounded-xl px-2 py-1 text-orange-500 bg-orange-300">Sketch</a>
+        </div>
+        <div class="describle">Im get 3 exp, develop new features SO YOU CAN in React.</div>
+        <button class="px-4 py-1 bg-violet-300 text-white rounded-xl">Connected</button>
+    </div>
+    <div style="width: 30%;" class="connect-item max-h-max mt-2 rounded-lg mx-3 px-3 py-4 bg-white flex flex-col items-center text-center">
+        <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+        <div class="con-name font-medium text-2xl">Mark Gilbert</div>
+        <div class="job-name text-zinc-500">UI Designner</div>
+        <div class="skill my-2">
+            <a href="#" class="rounded-xl px-2 py-1 text-slate-700 bg-slate-400">Figma</a>
+            <a href="#" class=" rounded-xl px-2 py-1 text-orange-500 bg-orange-300">Sketch</a>
+        </div>
+        <div class="describle">Im get 3 exp, develop new features SO YOU CAN in React.</div>
+        <button class="px-4 py-1 bg-violet-300 text-white rounded-xl">Connected</button>
+    </div>
+    <div style="width: 30%;" class="connect-item max-h-max mt-2 rounded-lg mx-3 px-3 py-4 bg-white flex flex-col items-center text-center">
+        <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+        <div class="con-name font-medium text-2xl">Mark Gilbert</div>
+        <div class="job-name text-zinc-500">UI Designner</div>
+        <div class="skill my-2">
+            <a href="#" class="rounded-xl px-2 py-1 text-slate-700 bg-slate-400">Figma</a>
+            <a href="#" class=" rounded-xl px-2 py-1 text-orange-500 bg-orange-300">Sketch</a>
+        </div>
+        <div class="describle">Im get 3 exp, develop new features SO YOU CAN in React.</div>
+        <button class="px-4 py-1 bg-violet-300 text-white rounded-xl">Connected</button>
+    </div>
+    <div style="width: 30%;" class="connect-item max-h-max mt-2 rounded-lg mx-3 px-3 py-4 bg-white flex flex-col items-center text-center">
+        <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+        <div class="con-name font-medium text-2xl">Mark Gilbert</div>
+        <div class="job-name text-zinc-500">UI Designner</div>
+        <div class="skill my-2">
+            <a href="#" class="rounded-xl px-2 py-1 text-slate-700 bg-slate-400">Figma</a>
+            <a href="#" class=" rounded-xl px-2 py-1 text-orange-500 bg-orange-300">Sketch</a>
+        </div>
+        <div class="describle">Im get 3 exp, develop new features SO YOU CAN in React.</div>
+        <button class="px-4 py-1 bg-violet-300 text-white rounded-xl">Connected</button>
+    </div>
+    <div style="width: 30%;" class="connect-item max-h-max mt-2 rounded-lg mx-3 px-3 py-4 bg-white flex flex-col items-center text-center">
+        <img class="w-20" style="border-radius: 40px;" src="https://demos.pixinvent.com/vuexy-html-admin-template/assets/img/avatars/3.png" alt="">
+        <div class="con-name font-medium text-2xl">Mark Gilbert</div>
+        <div class="job-name text-zinc-500">UI Designner</div>
+        <div class="skill my-2">
+            <a href="#" class="rounded-xl px-2 py-1 text-slate-700 bg-slate-400">Figma</a>
+            <a href="#" class=" rounded-xl px-2 py-1 text-orange-500 bg-orange-300">Sketch</a>
+        </div>
+        <div class="describle">Im get 3 exp, develop new features SO YOU CAN in React.</div>
+        <button class="px-4 py-1 bg-violet-300 text-white rounded-xl">Connected</button>
+    </div>
+</div>
+</div>
+        `;
+  }
+
